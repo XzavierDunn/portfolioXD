8fd8a1caa2bd4a3a1547038cec8b9ab75843b5f1
Xzavier Dunn <xmandunn@gmail.com>
2020-09-30 16:50:09 -0400

    more

--- Assets/JS/typeIn.js
+++ Assets/JS/typeIn.js
@@ -4,6 +4,14 @@ let text1 = "Hey, what's up?"
 let text2 = "I'm Xzavier."
 let count1 = 0;
 let count2 = 0;
+let count3 = 0;
+
+let projects = document.getElementById('projects');
+let archive = document.getElementById('archive');
+let right = document.getElementById('right');
+let left = document.getElementById('left');
+let projectText = "Project";
+let archiveText = "Archive";
 
 const type = () => {
     if (count1 < text1.length) {
@@ -14,9 +22,19 @@ const type = () => {
         type2.innerHTML += text2.charAt(count2);
         count2++;
         setTimeout(type, 50)
+    } else if (count3 < projectText.length) {
+        projects.innerHTML += projectText.charAt(count3);
+        archive.innerHTML += archiveText.charAt(count3);
+        count3++;
+        setTimeout(type, 50)
+    } else {
+        projects.innerHTML += "s";
+        right.classList.add("fa-long-arrow-alt-right");
+        left.classList.add("fa-long-arrow-alt-left");
     }
 }
 
 if (type1.innerHTML != text1) {
     type();
 }
+
